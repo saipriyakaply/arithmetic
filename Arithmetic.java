21411c43d71c47b6aa4351e51c3db823df71a5a1
saipriyakaply <kaplysaipriya_24it@student.vardhaman.org>
2025-09-18 15:43:50 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,6 +1,6 @@
 public class Arithmetic {
     public static void main(String[] args) {
-        int a = 10, b = 5;
+        int a = 10, b = 2;
         System.out.println("Addition = " + (a + b));
         System.out.println("Subtraction = " + (a - b));
         System.out.println("Multiplication = " + (a * b));
